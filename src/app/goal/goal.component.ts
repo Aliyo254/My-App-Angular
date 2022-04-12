e2c353d6d3d340a7e784ac3dd8006458a6023b6d
Aliyo254 <alinurali254@gmail.com>
2022-04-12 15:11:56 +0300

    Change from Dog to Cat

--- src/app/goal/goal.component.ts
+++ src/app/goal/goal.component.ts
@@ -23,7 +23,7 @@ export class GoalComponent implements OnInit {
   goals:Goal[]=[
     new Goal(1,'Watch finding nemo','Find an online version and watch merline find his son',new Date (2020,3,14)),
     new Goal(2,'Buy Snacks','I have to buy cookies for my parrot',new Date (2019,6,9)),
-    new Goal(3,'Get Dog food','Diana has her birthday coming up soon',new Date(2022,1,12)),
+    new Goal(3,'Get Cat food','Diana has her birthday coming up soon',new Date(2022,1,12)),
     new Goal(4,'Get new phone Case','Pupper likes expensive snacks',new Date (2019,0,18)),
     new Goal(5,'Solve math homework','Damn Math',new Date(2019,2,14)),
     new Goal(6,'Plot my world domination plan','Cause I am an evil overload',new Date(2030,3,14)),
